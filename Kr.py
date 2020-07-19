ac3f00f4529cbe4ec45cf464d1e1e6a3ad1bd8eb
Ivan1507 <iancurtis1507@mail.ru>
2020-07-19 18:21:11 +0300

    Solving MIPT tests

--- Kr.py
+++ Kr.py
@@ -1,2 +1,20 @@
-for i in range(8):
-    print('ggasd')
+n = int(input())
+A = [0] * n
+for i in range(n):
+    A[i] = int(input())
+mx = 0
+mr = 0
+c = 0
+for i in range(n):
+    for j in range(i+1,n):
+        if A[i] == A[j]:
+            c += 1
+    if c > mx:
+        mx = c
+        mr = A[i]
+    c = 0
+if n == 1:
+    print(A[0])
+else:
+    print(mr)
+
