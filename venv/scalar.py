cc1c1b4ed267cef3e439369440ce2250671db8ed
Ivan1507 <iancurtis1507@mail.ru>
2020-07-23 13:29:49 +0300

    Наибольшая общая подпоследовательность с восста ответа

--- venv/scalar.py
+++ venv/scalar.py
@@ -1,13 +1,26 @@
-price ,delta ,week = map(int,input().split())
-money = 0
-week *= 7
-t = price
-while week >0:
-    if week == 1:
-        price = t
+n = int(input())
+A = [int(a) for a in input().split()]
+l = int(input())
+B = [int(q) for q in input().split()]
+F=[[0]*(len(B)+1) for i in range(len(A)+1)]
+for i in range(1,n+1):
+    for j in range(1,l+1):
+        if A[i-1]==B[j-1]:
+            F[i][j]=F[i-1][j-1]+1
+        else:
+            F[i][j]=max(F[i-1][j],F[i][j-1])
+C = []
+i = n
+j = l
+while F[i][j]>0:
+    if F[i][j]==F[i-1][j]:
+        i -= 1
+    elif F[i][j]==F[i][j-1]:
+        j -= 1
     else:
-        price += delta
-    money += price
-    week -= 1
-print(money)
-
+        C.append(A[i-1])
+        i -= 1
+        j -= 1
+C.reverse()
+for i in range(0,F[-1][-1]):
+    print(C[i])
