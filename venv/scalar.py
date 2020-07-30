9e562cc7c74e287e6b534f209d26cb7d2c6cffc7
Ivan1507 <iancurtis1507@mail.ru>
2020-07-30 15:25:33 +0300

    Треугольник Паскаля

--- venv/scalar.py
+++ venv/scalar.py
@@ -1,26 +1,8 @@
 n = int(input())
-A = [int(a) for a in input().split()]
-l = int(input())
-B = [int(q) for q in input().split()]
-F=[[0]*(len(B)+1) for i in range(len(A)+1)]
-for i in range(1,n+1):
-    for j in range(1,l+1):
-        if A[i-1]==B[j-1]:
-            F[i][j]=F[i-1][j-1]+1
-        else:
-            F[i][j]=max(F[i-1][j],F[i][j-1])
-C = []
-i = n
-j = l
-while F[i][j]>0:
-    if F[i][j]==F[i-1][j]:
-        i -= 1
-    elif F[i][j]==F[i][j-1]:
-        j -= 1
-    else:
-        C.append(A[i-1])
-        i -= 1
-        j -= 1
-C.reverse()
-for i in range(0,F[-1][-1]):
-    print(C[i])
+A = [[1]*(n+1) for i in range(n+1)]
+for i in range(0,n+1):
+    for j in range(0,i+1):
+        if i>=2 and j<i and j>0:
+            A[i][j]=A[i-1][j]+A[i-1][j-1]
+        print(A[i][j],end=' ')
+    print()
