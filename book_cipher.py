e1692be4de14c1a80fb8f7e6eb98e743b7c13470
Ivan1507 <iancurtis1507@mail.ru>
2020-08-05 14:04:30 +0300

    backpack problem

--- book_cipher.py
+++ book_cipher.py
@@ -1,12 +1,22 @@
-def erotosfen(N):
-    A = []
-    for i in range(0, N+1):
-        A.append(True)
-    for i in range(2, N+1):
-        if A[i]:
-            for j in range(i+i,N+1,i):
-                A[j] = False
-    for i in range(2, N+1):
-        if A[i]:
-            print(i)
-erotosfen(int(input()))
+n = int(input())
+w = int(input())
+M =[]*n
+V = []*n
+for i in range(0,n):
+    k = int(input())
+    M.append(k)
+    v = int(input())
+    V.append(v)
+F = [[0]*(n+1) for i in range(w+1)]
+for i in range(1, n+1):
+    for k in range(1, w+1):
+        if M[i-1] <= k:
+            F[k][i] = max(F[k][i-1],V[i-1]+F[k-M[i-1]][i-1])
+        else:
+            F[k][i] = F[k][i-1]
+for i in range(1,n+1):
+    for j in range(1,w+1):
+        print(F[j][i],end=' ')
+    print()
+print(F[w][n])
+
